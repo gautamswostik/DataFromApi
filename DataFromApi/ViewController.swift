b1081705708bd1887297da7211b9866733da6c56
gautamswostik <swastikg11@gmail.com>
2022-11-20 09:56:05 +0545

    Initial Commit

---
+++ DataFromApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataFromApi
+//
+//  Created by swostik gautam on 20/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
